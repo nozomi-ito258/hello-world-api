df1974274db74ed5c4b4e1caf7d9b17d1fd904b7
nozomi-ito <nozomi.ito2@askul.com>
2021-10-14 00:24:06 +0900

    first commit

---
+++ src/main/kotlin/com/example/helloworldapi/HelloWorldApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.helloworldapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloWorldApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelloWorldApiApplication>(*args)
+}
